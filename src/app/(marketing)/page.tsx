2d9aa57c60fc1b523b09d63499c96bd3aa420877
devansu6789 <newid8974@gmail.com>
2023-12-25 11:52:04 +0530

    added landing page

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Heading } from "./_components/Heading";
+import { Heroes } from "./_components/heroes";
+import { Footer } from "./_components/footer";
+
+const MarketingPage = () => {
+  return (
+    <div className="min-h-full flex flex-col items-center justify-center">
+      <div className="flex flex-col item-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+        <Heroes />
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default MarketingPage;
